c0a27affde89852ef576964bee09e80f8ac8fd9e
Alvaro Tedeschi <alvarotedeschi@gmail.com>
2023-07-02 15:38:34 -0300

    fix user_store

--- db/postgres/user_store.go
+++ db/postgres/user_store.go
@@ -26,7 +26,7 @@ const getUsersQuery = `SELECT * FROM users`
 
 func (s *UserStore) GetUsers() ([]models.User, error) {
 	users := []models.User{}
-	if err := s.Get(&users, getUsersQuery); err != nil {
+	if err := s.Select(&users, getUsersQuery); err != nil {
 		return users, fmt.Errorf("error getting users: %w", err)
 	}
 	return users, nil
@@ -36,7 +36,7 @@ const createUserQuery = `INSERT INTO users (name) VALUES ($1) RETURNING *`
 
 func (s *UserStore) CreateUser(params models.UserCreateParams) (models.User, error) {
 	user := models.User{}
-	if err := s.Get(createUserQuery, params.Name); err != nil {
+	if err := s.Get(&user, createUserQuery, params.Name); err != nil {
 		return user, fmt.Errorf("error creating user: %w", err)
 	}
 	return user, nil
@@ -45,6 +45,10 @@ func (s *UserStore) CreateUser(params models.UserCreateParams) (models.User, err
 const deleteUserQuery = `DELETE FROM users WHERE id = $1`
 
 func (s *UserStore) DeleteUser(id uint64) error {
+    user := models.User{}
+	if err := s.Get(&user, getUserQuery, id); err != nil {
+		return fmt.Errorf("error deleting user: %w", err)
+	}
 	if _, err := s.Exec(deleteUserQuery, id); err != nil {
 		return fmt.Errorf("error deleting user: %w", err)
 	}
