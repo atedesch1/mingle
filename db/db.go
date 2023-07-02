38b4ac4efeb96600939013009b59b680d8edc4ff
Alvaro Tedeschi <alvarotedeschi@gmail.com>
2023-07-02 15:40:46 -0300

    fix pointing to postgres db

--- db/db.go
+++ db/db.go
@@ -3,5 +3,5 @@ package db
 import postgres "github.com/atedesch1/mingle/db/postgres"
 
 func NewStorage() (Storage, error) {
-	return postgres.NewPostgresStorage("postgresql://root:pass@0.0.0.0:5432/postgres?sslmode=disable")
+	return postgres.NewPostgresStorage("postgresql://root:pass@0.0.0.0:5432/mingle?sslmode=disable")
 }
